1172d8df355163cbc44a8b29fa95e493c8867d67
Jihn Nguyen <110032329+smoking-is-bad@users.noreply.github.com>
2024-08-17 11:37:37 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyBK23UrzojqTRxaEDfpaTVBP75jwTbNjxc",
+    authDomain: "friendlychat-18d30.firebaseapp.com",
+    databaseURL: "https://friendlychat-18d30-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "friendlychat-18d30",
+    storageBucket: "friendlychat-18d30.appspot.com",
+    messagingSenderId: "1048826450455",
+    appId: "1:1048826450455:web:35928d8ee7e155d1c2c252"
   },
 };
