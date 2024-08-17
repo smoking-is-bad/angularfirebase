e91b4f8af7c5686855f7d4416f2b291541f7d30e
Jihn Nguyen <110032329+smoking-is-bad@users.noreply.github.com>
2024-08-17 11:22:50 +0700

    Adding Google Authentication

---
+++ src/firebase-config.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBK23UrzojqTRxaEDfpaTVBP75jwTbNjxc",
+    authDomain: "friendlychat-18d30.firebaseapp.com",
+    databaseURL: "https://friendlychat-18d30-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "friendlychat-18d30",
+    storageBucket: "friendlychat-18d30.appspot.com",
+    messagingSenderId: "1048826450455",
+    appId: "1:1048826450455:web:35928d8ee7e155d1c2c252"
+  };
